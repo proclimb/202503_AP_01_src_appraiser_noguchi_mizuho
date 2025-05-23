7324a64f55b415b3827c261cd051fe152417a93d
m.noguchi <202503ap01_noguchi@x-ide.sakura.ne.jp>
2025-05-23 14:57:58 +0900

    #40316　面積の値が整数以下なのに、エラーメッセージが表示されない

--- SRC/js/stock.js
+++ SRC/js/stock.js
@@ -31,7 +31,7 @@ function fnStockEditCheck() {
 	}
 
 	tmp = form.area.value;
-	if (tmp.length > 6 || tmp.match(/[^0-9\.]+/)) {
+	if (tmp.length > 0 && !tmp.match(/^([1-9][0-9]{0,2}|0)?$/)) {
 		alert('面積は3桁以内（小数点以下2桁以内）の半角数字で入力してください');
 		return;
 	}
