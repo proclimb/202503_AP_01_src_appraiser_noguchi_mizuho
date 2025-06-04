12ea47f093bcff19a9820a9f7fb329c8d12e15ea
m.noguchi <202503ap01_noguchi@x-ide.sakura.ne.jp>
2025-06-04 12:41:34 +0900

    #40593　物件名を入力しているのに、エラーメッセージが出る

--- SRC/js/article.js
+++ SRC/js/article.js
@@ -3,14 +3,16 @@
 //
 function fnArticleEditCheck() {
 	tmp = form.article.value;
-	if (tmp) {
+	if (tmp.length == 0) {
 		alert('物件名を入力してください');
 		return;
 	}
 	if (isLength(100, "物件名", form.article)) { return; }
 
-	form.act.value = 'articleEditComplete';
-	form.submit();
+	if (confirm('この内容で登録します。よろしいですか？')) {
+		form.act.value = 'articleEditComplete';
+		form.submit();
+	}
 }
 
 
