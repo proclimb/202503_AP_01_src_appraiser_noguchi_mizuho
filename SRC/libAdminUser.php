a50d9cf9c76b206c6383eb6dff5b4afbc855e537
m.noguchi <202503ap01_noguchi@x-ide.sakura.ne.jp>
2025-07-01 16:41:57 +0900

    #41363　adminユーザの削除リンクが有効化している

--- SRC/libAdminUser.php
+++ SRC/libAdminUser.php
@@ -44,7 +44,9 @@ function subAdminUser()
 						<td class="list_td<?php print $i; ?>"><?php print $password; ?></td>
 						<td class="list_td<?php print $i; ?>"><?php print fnAuthorityName($authority); ?></td>
 						<td class="list_td<?php print $i; ?>">
-							<a href="javascript:fnAdminUserDeleteCheck(<?php print $userNo; ?>,'<?php print $name; ?>');">削除</a>
+							<?php if ($userNo > 1) { ?>
+								<a href="javascript:fnAdminUserDeleteCheck(<?php print $userNo; ?>,'<?php print $name; ?>');">削除</a>
+							<?php } ?>
 						</td>
 					</tr>
 				<?php
